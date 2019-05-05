2ddfde5dd2abaf7c396ee9e02e10b9b811173ddc
Alex Radu <alex_radu@live.com>
2019-05-05 11:31:37 +0300

    SB incomplete

---
+++ TheCrawler/Ops_SB.cs
@@ -0,0 +1,38 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TheCrawler
+{
+    public partial class Ops
+    {
+        public List<Match> FetchMatches_SB()
+        {
+            ChromeDriver browser = new ChromeDriver();
+
+            List<Match> lsFootball = new List<Match>();
+
+            browser.Navigate().GoToUrl($"https://agentii.stanleybet.ro/sportsbetting/Fotbal/68");
+
+            Thread.Sleep(3000);
+
+            IWebElement button = browser.FindElement(By.CssSelector("[class='successButton ui-button ui-widget ui-state-default ui-corner-all ui-button-text-only']"));
+
+            button.Clear();
+
+            IWebElement games = browser.FindElement(By.CssSelector("[class='oddsTable drop-shadow lifted']"));
+
+            browser.Dispose();
+
+            return lsFootball;
+        }
+    }
+}
